cf8dcddf01479913e07ffe59a17ead6c24c4fbcc
ss1271 <ss1271@gmail.com>
2013-10-11 17:14:15 +0100

    Update with sensor detection.

--- src/com/yeyaxi/android/sensorfun/MainActivity.java
+++ src/com/yeyaxi/android/sensorfun/MainActivity.java
@@ -1,15 +1,29 @@
 package com.yeyaxi.android.sensorfun;
 
-import android.os.Bundle;
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	
+	private SensorManager mSensorManager;
+	private List<Sensor> deviceSensors;
+	private static final String TAG = MainActivity.class.getSimpleName();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		detectSensors();
 	}
 
 	@Override
@@ -19,4 +33,43 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		
+		
+	}
+	
+	
+	private void detectSensors() {		
+		deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+		
+		Log.d(TAG, "" + deviceSensors);
+		
+		if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null)
+			Log.i(TAG, "Accelerometer");
+		if (mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE) != null)
+			Log.i(TAG, "Ambient Temperature");
+		if (mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY) != null)
+			Log.i(TAG, "Gravity");
+		if (mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null)
+			Log.i(TAG, "Gyroscope");
+		if (mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) != null)
+			Log.i(TAG, "Light");
+		if (mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) != null)
+			Log.i(TAG, "Linear Acceleration");
+		if (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null)
+			Log.i(TAG, "Magnetic Field");
+//		if (mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION) != null)
+//			Log.i(TAG, "Orientation");
+		if (mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE) != null)
+			Log.i(TAG, "Pressure");
+		if (mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY) != null)
+			Log.i(TAG, "Proximity");
+		if (mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY) != null)
+			Log.i(TAG, "Relative Humidity");
+		if (mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) != null)
+			Log.i(TAG, "Rotation Vector");
+	}
 }
