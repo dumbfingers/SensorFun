7297c79fbabde9a601d2caac2ee260dcedef1618
ss1271 <ss1271@gmail.com>
2013-10-14 16:53:13 +0100

    Add Listener

--- src/com/yeyaxi/android/sensorfun/MainActivity.java
+++ src/com/yeyaxi/android/sensorfun/MainActivity.java
@@ -3,10 +3,15 @@ package com.yeyaxi.android.sensorfun;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -31,6 +36,11 @@ public class MainActivity extends Activity {
 	private TextView relatHumidTextView;
 	private TextView rotVecTextView;
 	
+	private SensorService mBoundService;
+	private boolean isBind = false;
+	
+	private BroadcastReceiver mReceiver;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -48,8 +58,15 @@ public class MainActivity extends Activity {
 		relatHumidTextView = (TextView) findViewById(R.id.textRelaHumid);
 		rotVecTextView = (TextView) findViewById(R.id.textRotVect);
 		
-		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-		detectSensors();
+		mReceiver = new BroadcastReceiver() {
+			
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				float[] accFloats = intent.getFloatArrayExtra("accelerometer");
+				accelerometerTextView.append(" :" + Float.toString(accFloats[0]) + Float.toString(accFloats[1]) + Float.toString(accFloats[2]));
+				
+			}
+		};
 	}
 
 	@Override
@@ -63,10 +80,48 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onStart() {
 		super.onStart();
-		
-		
 	}
 	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		doBindService();
+	}
+	
+	@Override
+	protected void onPause() {
+	  super.onPause();
+	  doUnbindService();
+	}
+	
+	
+	private ServiceConnection mConnection = new ServiceConnection() {
+		
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			// TODO Auto-generated method stub
+			mBoundService = ((SensorService.SensorBinder) service).getService();
+//			mBoundService.getMsg();
+			mSensorManager = mBoundService.getSensorManager();
+			detectSensors();
+		}
+	};
+	
+	private void doBindService() {
+		bindService(new Intent(this, SensorService.class), mConnection, Context.BIND_AUTO_CREATE);
+		isBind = true;
+	}
+	
+	private void doUnbindService() {
+		unbindService(mConnection);
+		isBind = false;
+	}
 	
 	private void detectSensors() {		
 		deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
@@ -76,36 +131,49 @@ public class MainActivity extends Activity {
 		if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
 //			Log.i(TAG, "Accelerometer");
 			accelerometerTextView.setVisibility(View.VISIBLE);
+//			mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
 		}
 		
 		if (mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE) != null) {
 //			Log.i(TAG, "Ambient Temperature");
 			ambTempTextView.setVisibility(View.VISIBLE);
+//			mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE), SensorManager.SENSOR_DELAY_NORMAL);
+
 		}
 		
 		if (mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY) != null) {
 //			Log.i(TAG, "Gravity");
 			gravityTextView.setVisibility(View.VISIBLE);
+//			mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_NORMAL);
+
 		}
 		
 		if (mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null) {
 //			Log.i(TAG, "Gyroscope");
 			gyroTextView.setVisibility(View.VISIBLE);
+//			mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
+
 		}
 		
 		if (mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) != null) {
 //			Log.i(TAG, "Light");
 			lightTextView.setVisibility(View.VISIBLE);
+//			mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT), SensorManager.SENSOR_DELAY_NORMAL);
+
 		}
 		
 		if (mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) != null) {
 //			Log.i(TAG, "Linear Acceleration");
 			linearAccTextView.setVisibility(View.VISIBLE);
+//			mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION), SensorManager.SENSOR_DELAY_NORMAL);
+
 		}
 		
 		if (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null) {
 //			Log.i(TAG, "Magnetic Field");
 			magneticTextView.setVisibility(View.VISIBLE);
+//			mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_NORMAL);
+
 		}
 		
 //		if (mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION) != null)
@@ -113,21 +181,29 @@ public class MainActivity extends Activity {
 		if (mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE) != null) {
 //			Log.i(TAG, "Pressure");
 			pressureTextView.setVisibility(View.VISIBLE);
+//			mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE), SensorManager.SENSOR_DELAY_NORMAL);
+
 		}
 		
 		if (mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY) != null) {
 //			Log.i(TAG, "Proximity");
 			proxiTextView.setVisibility(View.VISIBLE);
+//			mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY), SensorManager.SENSOR_DELAY_NORMAL);
+
 		}
 		
 		if (mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY) != null) {
 //			Log.i(TAG, "Relative Humidity");
 			relatHumidTextView.setVisibility(View.VISIBLE);
+//			mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY), SensorManager.SENSOR_DELAY_NORMAL);
+
 		}
 		
 		if (mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) != null) {
 //			Log.i(TAG, "Rotation Vector");
 			rotVecTextView.setVisibility(View.VISIBLE);
+//			mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR), SensorManager.SENSOR_DELAY_NORMAL);
+
 		}
 	}
 }
